3c7a5eb0879d5ddb3ceb430fd12168bfad3264c7
and <77030548+exception7601@users.noreply.github.com>
2025-04-29 18:39:46 -0300

    add new configs

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -5,28 +5,28 @@ return {
     -- Existe um linter genérico chado compilerque usa o 
     -- makeprge errorformatopções do buffer atual. 
 
-    -- local lint = require("lint")
+    local lint = require("lint")
 
-    --  lint.linters_by_ft = {
-    --    lua = { 'luacheck' },
-    --    swift = { 'swiftlint' },
-    --  }
-    --  local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    lint.linters_by_ft = {
+      -- lua = { 'luacheck' },
+      swift = { 'swiftlint' },
+    }
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    --  -- Auto lint
-    --  vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-    --    group = lint_augroup,
-    --    callback = function()
-    --      lint.try_lint()
-    --    end,
-    --  })
+    -- Auto lint
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
 
-    --  -- Criando o comando 'LintTryLint' no Neovim
-    --  vim.api.nvim_create_user_command(
-    --  'LintTryLint',
-    --  function ()
-    --    lint.try_lint()
-    --  end, { desc = "Trigger linting for current file" }
-    --  )
+    -- Criando o comando 'LintTryLint' no Neovim
+    vim.api.nvim_create_user_command(
+      'LintTryLint',
+      function ()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" }
+    )
   end
 }
